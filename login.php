544371f8eda551a270f42d7628d17881935b5736
Nikolay <johndoe@example.com>
2022-03-30 13:17:29 +0700

    abc.2

--- login.php
+++ login.php
@@ -1,31 +1,29 @@
 <?php
-    session_start();
-    if(!empty($_POST['name']) && !empty($_POST['pas'])){
-    	$name = $_POST ['name'];
-    	$password = $_POST['pas'];
-    	//авторизацмя успешна 
-    	if ($name === 'qwe' && $password === '123'){
-    		$_SESSION['user_name']=$name;
-    		header('Location: index.php');
-      	}
-    }
+require_once 'user.php';
+$user = new User();
+$user->set('qwe', 'admin', '123');
+if ($user->authorize($_POST) === true) {
+	$_SESSION['authorized'] = 1;
+	$user->saveToSession();
+	echo '<script>window.location="index.php"</script>';
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset = "UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Авторизация</title>
-	<link rel= "stylesheet" href="style.css">
-</head>
-<body>
-	<div id="content">
-		<form action ="" method="post">
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Авторизация</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div id="content"
+		<from action="" method="post">
 			<h3>Авторизация</h3>
-			<input type="text" name="name" placeholder="Логин"><br>
-			<input type="text" name="pas" placeholder="пароль"><br>
-			<input type="submit" name="">
-		</form>
-	</div>
-</body>
-</html>
+			<input type="text" name="login" placeholder="Логин"><dr>
+				<input type="text" name="pas" placeholder="Пароль"><br>
+				<input type="submit" name="">
+			</from>
+		</div>
+	</body>
+	</html>
